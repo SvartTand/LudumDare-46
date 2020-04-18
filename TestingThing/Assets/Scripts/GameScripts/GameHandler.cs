54f388a905684005b568d9de9dd6ea7eab75c32b
SvartTand <albert24lindberg@gmail.com>
2020-04-18 18:37:45 +0300

    Had to  revert to 2018 version of unity and made the planet almost complete

---
+++ TestingThing/Assets/Scripts/GameScripts/GameHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour {
+
+
+    public Planet planet;
+	// Use this for initialization
+	void Start () {
+        planet.GeneratePlanet();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
