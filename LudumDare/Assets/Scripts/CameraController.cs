172dacbd5a4e368c8491e31b668c6ddc4d992f99
SvartTand <albert24lindberg@gmail.com>
2020-04-18 11:35:48 +0300

    Added camera

---
+++ LudumDare/Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform planet;
+    public float rotationSpeed;
+    public float zoomSpeed;
+    public float minFov;
+    public float maxFov;
+    public float sensitivity;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButton(1))
+        {
+            transform.RotateAround(planet.position, transform.up, Input.GetAxis("Mouse X") * rotationSpeed);
+            transform.RotateAround(planet.position, transform.right, Input.GetAxis("Mouse Y") * -rotationSpeed);
+        }
+
+        float fov = Camera.main.fieldOfView;
+        fov += Input.GetAxis("Mouse ScrollWheel") * -sensitivity;
+        fov = Mathf.Clamp(fov, minFov, maxFov);
+        Camera.main.fieldOfView = fov;
+    }
+}
